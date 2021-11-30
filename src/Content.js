611466804d097d2365b5ad17603bb58471221726
Naved <navedatgames@gmail.com>
2021-11-30 13:11:35 +0530

    custom components and jsx

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+const page = (<p>
+                this is my main myContent
+            </p>)
+let Content = ()=>{
+    return(
+      page
+    )
+  }
+  
+  
+  
+  
+  export default Content;
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+//this is jsx
+let nav = (
+    <nav>
+        <h1>Websites</h1>
+        <ul>
+            <li>Goods</li>
+            <li>Services</li>
+            <li>Packags</li>
+            <li>something</li>
+        </ul>
+    </nav>
+)
+let Navbar = () =>{
+    return (
+     nav
+    );
+  }
+
+  export default Navbar;
