af3f680f74521b4c64358bb00fa5e1ed15683739
Naved <navedatgames@gmail.com>
2021-11-30 15:22:33 +0530

    created a static page in react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Navbar from "./Navbar"
-import Content from "./Content"
+//lets create jsx
+
+let page = (
+  <>
+    <img src = "logo512.png" alt = "Not found" width = "100px" ></img>
+    <h1>
+      Fun facts about React
+    </h1>
+    <ul>
+      <li>was released in 2013</li>
+      <li>was originally creted by Jordan Walke</li>
+      <li>has well over 100k stars on github</li>
+      <li>maintained by Facebook</li>
+      <li>Power 1000 of enterprise apps </li>
+
+    </ul>
+  </>
+)
 
 let App = ()=>{
   return (
-    <>
-    <Navbar/>
-    <Content />
-    </>
+    page
   )
 }
 
